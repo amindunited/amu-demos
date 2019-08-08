009aac9cee0b74289a50b8cf48f457e067cefe2a
Robin Buckley <robin.buckley@auspost.com.au>
2019-08-08 14:23:32 +1000

    Added React tv

---
+++ packages/react-tv/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="App-header">
+      App Header
+    </header>
+  );
+}
+
+export default Header;
